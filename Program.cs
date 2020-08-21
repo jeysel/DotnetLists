b558f4951e4729221228236683dddec7401b90bd
Jeysel Pacheco Bastos <jeysel@gmail.com>
2020-08-21 08:11:59 -0300

    Fim

--- Program.cs
+++ Program.cs
@@ -8,13 +8,6 @@ namespace DotnetLists
         {
             Console.Clear();
 
-            var meuArray = new int[5] { 1, 2, 3, 4, 5 };
-            meuArray[0] = 12;
-
-            //Console.WriteLine(meuArray[0]);
-
-            Console.WriteLine(meuArray.Length);
-
             /*
             //percorrendo o array
             for (var index = 0; index < meuArray.Length; index++)
@@ -23,14 +16,56 @@ namespace DotnetLists
             }
             */
 
-
+            /*
             // foreach
+            var meuArray = new int[5] { 1, 2, 3, 4, 5 };
+            meuArray[0] = 12;
+
+            //Console.WriteLine(meuArray[0]);
+
+            Console.WriteLine(meuArray.Length);
             foreach (var item in meuArray)
             {
                 Console.WriteLine(item);
             }
+            */
+
+            // tratamento de erro
+
+            var arr = new int[3];
+
+            try
+            {
+                for (var index = 0; index < 10; index++)
+                {
+                    Console.WriteLine(arr[index]);
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.InnerException);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Indice não encontrado.");
+
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Ops, algo deu errado!");
+            }
 
 
         }
+
+        static void Cadastrar(string texto)
+        {
+            if (string.IsNullOrEmpty(texto))
+                throw new Exception("Texto não pode ser vazio");
+        }
+
+
     }
+
 }
